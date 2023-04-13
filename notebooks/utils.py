d1491c673e5d52c359299f08ad273cd44323f4a0
Jordan Pownall <jord_pownall@hotmail.co.uk>
2023-04-13 18:50:54 +0100

    Fixed bugs. Works now, also selects a vice captain

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -15,17 +15,17 @@ def switch_player_entry(entry, df_to_add, df_to_drop):
     df_to_add - the dataframe with the added player
     df_to_drop - the dataframe with the dropped player
     """
+    # Store the original index of the entry before resetting it
+    original_index = entry.index
+
     # Ensure the entry has a unique index before adding it to df_to_add
     entry = entry.reset_index(drop=True)
 
     # Add the entry to df_to_add and reset the index
     df_to_add = pd.concat([df_to_add, entry]).reset_index(drop=True)
 
-    # Find the index of the entry in df_to_drop
-    index_to_drop = df_to_drop[df_to_drop.index.isin(entry.index)].index
-
-    # Drop the entry from df_to_drop using the found index and reset the index
-    df_to_drop = df_to_drop.drop(index_to_drop).reset_index(drop=True)
+    # Drop the entry from df_to_drop using the original index and reset the index
+    df_to_drop = df_to_drop.drop(original_index).reset_index(drop=True)
 
     return df_to_add, df_to_drop
 
@@ -33,7 +33,7 @@ def switch_player_entry(entry, df_to_add, df_to_drop):
 def select_initial_starting_11(team, variable):
     team = team.sort_values(variable, ascending=False)
     starting_df = pd.DataFrame()
-    sub_df = pd.DataFrame()
+    subs_df = pd.DataFrame()
 
     # Select 1 goalkeeper
     gk = team[team.position == "GK"].head(1)
@@ -43,81 +43,90 @@ def select_initial_starting_11(team, variable):
     defs = team[team.position == "DEF"].head(3)
     starting_df, team = switch_player_entry(defs, starting_df, team)
 
+    # Select at least 3 midfielders
+    mids = team[team.position == "MID"].head(3)
+    starting_df, team = switch_player_entry(mids, starting_df, team)
+
     # Select at least 1 forward
     fwd = team[team.position == "FWD"].head(1)
     starting_df, team = switch_player_entry(fwd, starting_df, team)
 
-    # Select remaining positions
-    remaining_positions = 7
-    positions_selected = [p.position for p in starting_df.itertuples()]
-    while remaining_positions > 0:
-        if positions_selected.count("DEF") < 3:
-            # Select defender if less than 3 defenders are already selected
-            selected = team[team.position == "DEF"].head(1)
-            starting_df, team = switch_player_entry(selected, starting_df, team)
-            positions_selected.append("DEF")
-            remaining_positions -= 1
-        elif positions_selected.count("FWD") < 1:
-            # Select forward if less than 1 forward is already selected
-            selected = team[team.position == "FWD"].head(1)
-            starting_df, team = switch_player_entry(selected, starting_df, team)
-            positions_selected.append("FWD")
-            remaining_positions -= 1
-        else:
-            # Select highest predicted points player from remaining players
-            selected = team.head(1)
-            starting_df, team = switch_player_entry(selected, starting_df, team)
-            positions_selected.append(selected.iloc[0].position)
-            remaining_positions -= 1
+    # Fill the remaining positions with the highest predicted points players, excluding goalkeepers
+    remaining_positions = 3
+    top_players = team[team.position != "GK"].head(remaining_positions)
+    starting_df, team = switch_player_entry(top_players, starting_df, team)
 
-    sub_df = team
+    subs_df = team
 
-    # Print formation
-    defenders_count = (starting_df.position == "DEF").sum()
-    midfielders_count = (starting_df.position == "MID").sum()
-    forwards_count = (starting_df.position == "FWD").sum()
-    print(f"Formation is {defenders_count}-{midfielders_count}-{forwards_count}")
-
-    return starting_df, sub_df
+    return starting_df, subs_df
 
 
-def select_captain(players_df):
-    print("-----------------------CAPTAIN-----------------------")
+def select_captain_and_vice_captain(players_df):
+    print("-----------------------CAPTAIN & VICE-CAPTAIN-----------------------")
     # Find the index of the player with the highest predicted_points
     highest_predicted_points_index = players_df['predicted_points'].idxmax()
 
-    # Double the total_points value for that player
-    players_df.loc[highest_predicted_points_index, 'total_points'] *= 2
+    # Find the index of the player with the second highest predicted_points
+    players_no_captain = players_df.drop(highest_predicted_points_index)
+    second_highest_predicted_points_index = players_no_captain['predicted_points'].idxmax()
 
-    # Print the player with the highest predicted points
     captain_player = players_df.loc[highest_predicted_points_index]
-    print(f"Selected captain: {captain_player['name']} with predicted points {captain_player['predicted_points'] }"
-          f"and actual points {captain_player['total_points']/2} giving {captain_player['total_points']} points")
+    vice_captain_player = players_df.loc[second_highest_predicted_points_index]
+
+    # Check if the captain has played that week
+    if captain_player['minutes'] == 0:
+        print(f"Captain {captain_player['name']} has not played this week.")
+        # Double the total_points value for the vice-captain
+        players_df.loc[second_highest_predicted_points_index, 'total_points'] *= 2
+        print(
+            f"Selected vice-captain: {vice_captain_player['name']} with predicted points {vice_captain_player['predicted_points']}"
+            f" and actual points {vice_captain_player['total_points']} giving {vice_captain_player['total_points']*2} points")
+    else:
+        # Double the total_points value for the captain
+        players_df.loc[highest_predicted_points_index, 'total_points'] *= 2
+        print(f"Selected captain: {captain_player['name']} with predicted points {captain_player['predicted_points']}"
+              f" and actual points {captain_player['total_points']} giving {captain_player['total_points']*2} points")
+        print(f"Selected vice-captain: {vice_captain_player['name']} with predicted points {vice_captain_player['predicted_points']}")
 
     # Return the modified DataFrame
     return players_df
 
 
-def find_suitable_sub(earliest_kickoff_player, players_df, subs_df):
+def get_available_positions_to_sub(earliest_kickoff_player, players_df):
     gk_count = (players_df.position == "GK").sum()
     def_count = (players_df.position == "DEF").sum()
     mid_count = (players_df.position == "MID").sum()
     fwd_count = (players_df.position == "FWD").sum()
 
+    available_positions = []
+
+    if earliest_kickoff_player['position'] == 'GK':
+        available_positions.append('GK')
+    else:
+        if earliest_kickoff_player['position'] == 'DEF':
+            def_count -= 1
+        elif earliest_kickoff_player['position'] == 'MID':
+            mid_count -= 1
+        elif earliest_kickoff_player['position'] == 'FWD':
+            fwd_count -= 1
+
+        if def_count + 1 >= 3:
+            available_positions.append('DEF')
+        if mid_count + 1 >= 3:
+            available_positions.append('MID')
+        if fwd_count + 1 >= 1:
+            available_positions.append('FWD')
+
+    return available_positions
+
+
+def find_suitable_sub(earliest_kickoff_player, players_df, subs_df):
+    available_positions = get_available_positions_to_sub(earliest_kickoff_player, players_df)
     suitable_subs = []
 
     for _, sub in subs_df.iterrows():
-        if earliest_kickoff_player['position'] == 'GK' and gk_count > 1:
-            if sub['position'] != 'GK':
-                suitable_subs.append(sub)
-        elif earliest_kickoff_player['position'] == 'DEF' and def_count > 3:
+        if sub['position'] in available_positions:
             suitable_subs.append(sub)
-        elif earliest_kickoff_player['position'] == 'MID' and mid_count > 1:
-            if sub['position'] != 'MID':
-                suitable_subs.append(sub)
-        elif earliest_kickoff_player['position'] == 'FWD' and fwd_count > 1:
-            if sub['position'] != 'FWD':
-                suitable_subs.append(sub)
 
     if suitable_subs:
         # Sort the list by predicted_points in descending order and return the first element
@@ -142,7 +151,8 @@ def sub_not_played_player(players_df, subs_df):
 
     earliest_kickoff_player = zero_minutes_players.loc[zero_minutes_players['kickoff_time'].idxmin()]
 
-    # Find a player in the subs_df that can be switched with the earliest_kickoff_player while satisfying the formation constraints
+    # Find a player in the subs_df that can be switched with the earliest_kickoff_player while satisfying the
+    # formation constraints
     suitable_sub = find_suitable_sub(earliest_kickoff_player, players_df, subs_df)
 
     # Check if a switchable sub is found
@@ -151,19 +161,44 @@ def sub_not_played_player(players_df, subs_df):
 
         # Convert earliest_kickoff_player and switchable_sub to DataFrames
         earliest_kickoff_player_df = earliest_kickoff_player.to_frame().T
-        switchable_sub_df = suitable_sub.to_frame().T
+        suitable_sub_df = suitable_sub.to_frame().T
 
         # Switch the earliest_kickoff_player with the switchable_sub
-        players_df, subs_df = switch_player_entry(earliest_kickoff_player_df, subs_df, players_df)
-        subs_df, players_df = switch_player_entry(switchable_sub_df, players_df, subs_df)
+        subs_df, players_df = switch_player_entry(earliest_kickoff_player_df, subs_df, players_df)
+        players_df, subs_df = switch_player_entry(suitable_sub_df, players_df, subs_df)
     else:
         print("No suitable substitution found.")
 
     return players_df, subs_df
 
 
+def validate_squad_sizes(starting_df, subs_df):
+    starting_count = starting_df.shape[0]
+    subs_count = subs_df.shape[0]
+
+    if starting_count != 11 and subs_count != 4:
+        raise ValueError(f"The starting_df should have 11 players, but it has {starting_count} players. Also,"
+                         f"The subs_df should have 4 players, but it has {subs_count} players.\n"
+                         f"{display_team(starting_df, subs_df)}")
+
+    if starting_count != 11:
+        raise ValueError(f"The starting_df should have 11 players, but it has {starting_count} players.")
+
+    if subs_count != 4:
+        raise ValueError(f"The subs_df should have 4 players, but it has {subs_count} players.")
+
+    print("Squad size validation passed. Starting 11 and subs have correct number of players.")
+
 
 def display_team(starting_df, subs_df):
+    # Count the number of DEFs, MIDs, and FWDs in the starting_df
+    def_count = (starting_df.position == "DEF").sum()
+    mid_count = (starting_df.position == "MID").sum()
+    fwd_count = (starting_df.position == "FWD").sum()
+
+    # Print the formation
+    print(f"Formation: {def_count}-{mid_count}-{fwd_count}")
+
     print("STARTING:")
     print(starting_df)
     print("SUBS:")
@@ -172,8 +207,9 @@ def display_team(starting_df, subs_df):
 
 def organise_team(players_df, variable):
     starting_df, subs_df = select_initial_starting_11(players_df, variable)
+    starting_df = select_captain_and_vice_captain(starting_df)
     starting_df, subs_df = sub_not_played_player(starting_df, subs_df)
-    starting_df = select_captain(starting_df)
+    validate_squad_sizes(starting_df, subs_df)
     display_team(starting_df, subs_df)
 
     return starting_df, subs_df
