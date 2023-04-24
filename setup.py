9c951b9c8cba214dcc39d08dd6ef6ae7565664b6
Jordan Pownall <jord_pownall@hotmail.co.uk>
2023-04-24 11:42:51 +0100

    reformatting

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='This is a repository to store my dissertation project. This entails using AI techniques to build and alter a fantasy football squad.',
+    description='A repository for my dissertation project, Automated Fantasy Football Payer. The project contains files to analyse variables individual effects on players, joined when put into a parameterised model and also can simulate a season making changes based on predicted points.',
     author='Jordan Pownall',
     license='',
 )
